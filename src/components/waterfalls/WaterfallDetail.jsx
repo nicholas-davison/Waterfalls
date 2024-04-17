a9ef989f326ac4fc8954e659d30d2dc031f8d4aa
Nicholas Davison <nick.a.davison@gmail.com>
2024-04-17 11:57:26 -0500

    basic info on detail view including add favorite btuton

--- src/components/waterfalls/WaterfallDetail.jsx
+++ src/components/waterfalls/WaterfallDetail.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getWaterfallById } from "../../services/WaterfallService"
+import { Button, Col, Container, Image } from "react-bootstrap"
+
 
 export const WaterfallDetail = () => {
     const { waterfallId } = useParams()
@@ -8,11 +10,29 @@ export const WaterfallDetail = () => {
 
     useEffect(() => {
         getWaterfallById(waterfallId).then(res => setCurrentWaterfall(res))
-    }, [])
-    
+    }, [waterfallId])
+
     return (
         <div>
-            {waterfallId}
+            <Container>
+                <Col xs={10} md={8} className="image-container">
+                    {currentWaterfall ? (
+                        <Image src={currentWaterfall.imageUrl} fluid />
+                        ) : (
+                        <p>Loading image...</p> // Render a loading message
+                    )}
+                </Col>
+            </Container>
+            <Container>
+                <div className="waterfall-detail-header">
+                    <h1>{currentWaterfall.name}</h1>
+                    <Button variant="success">
+                        Add to Favorites
+                    </Button>
+                </div>
+                <h2>{currentWaterfall.location?.name}</h2>
+                <p>{currentWaterfall.description}</p>
+            </Container>
         </div>
     )
 }
