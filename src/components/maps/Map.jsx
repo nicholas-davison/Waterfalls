00e39bbb4c1cb7dd4bbd2eb0b004d9bf7428fff8
Nicholas Davison <nick.a.davison@gmail.com>
2024-04-23 11:22:14 -0500

    google maps place search active in new location component. saving place name, lat, and lng

--- src/components/maps/Map.jsx
+++ src/components/maps/Map.jsx
@@ -2,17 +2,19 @@ import React, { useEffect } from 'react';
 
 let map;
 
+const initMap = async () => {
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker")
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 35.925, lng: -86.580 },
+    zoom: 7,
+    mapTypeId: "terrain"
+  });
+}
 export const Map = () => {
   useEffect(() => {
-    const initMap = async () => {
-      const { Map } = await google.maps.importLibrary("maps");
-
-      map = new Map(document.getElementById("map"), {
-        center: { lat: 35.925, lng: -86.580 },
-        zoom: 7,
-      });
-    }
-
+    
+    
     initMap();
   }, []);
 
---
+++ src/components/maps/MapSearch.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef } from 'react';
+
+export const MapSearch = ({ newLocationInput, setNewLocationInput, inputRef }) => {
+  const mapRef = useRef(null); // Ref for the map container
+
+  useEffect(() => {
+    const initMap = async () => {
+      const { Map } = await google.maps.importLibrary("maps");
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+      const map = new Map(mapRef.current, {
+        center: { lat: 35.925, lng: -86.580 },
+        zoom: 7,
+        mapTypeId: "terrain"
+      });
+
+      const searchBox = new window.google.maps.places.SearchBox(inputRef.current);
+
+      searchBox.addListener('places_changed', () => {
+        const places = searchBox.getPlaces();
+        if (places.length === 0) {
+          return;
+        }
+        const place = places[0];
+        if (map) {
+          map.setCenter(place.geometry.location);
+          map.setZoom(15);
+        }
+/*         setClickedLocation({
+          latitude: place.geometry.location.lat(),
+          longitude: place.geometry.location.lng(),
+        }); */
+          const locationInputCopy = {...newLocationInput}
+          locationInputCopy.lat = place.geometry.location.lat(),
+          locationInputCopy.lng = place.geometry.location.lng()
+          locationInputCopy.name = place.name
+          setNewLocationInput(locationInputCopy)
+    });
+
+      // Clean up event listener when component unmounts
+      return () => {
+        window.google.maps.event.clearInstanceListeners(searchBox);
+      };
+    };
+
+    initMap();
+  }, [setNewLocationInput, inputRef]);
+
+  return (
+    <>
+      {/* Use the ref for the map container */}
+      <div ref={mapRef} id="map" style={{ width: '90%', height: '400px', margin: '0 auto'}}></div>
+    </>
+  );
+};
+
+  
